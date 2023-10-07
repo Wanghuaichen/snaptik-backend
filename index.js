ec07061409f1d21efd60e90345b873605ad9d5d6
shivaaydesigners <shivaayinfosoft@gmail.com>
2023-10-07 14:16:43 +0530

    changed host name

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ app.use(express.json());
 
 app.use("/snaptik", downloadRoutes);
 
-const HOST_NAME = 'https://snaptik.zone/'
-// const HOST_NAME = '68.178.145.77'
-const port = 8001;
+const HOST_NAME = 'http://snaptik.zone'
+// const HOST_NAME = 'https://68.178.145.77'
+const port = 5500;
 
 httpServer.listen(port, HOST_NAME, () => console.log(`Server listening on port ${port}`));
 
